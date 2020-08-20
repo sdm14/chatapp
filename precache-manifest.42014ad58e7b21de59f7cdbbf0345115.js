5abfcd26f5109fc9515b40abdad8a3be5b59fb88
sdm14 <149sdm10@gmail.com>
2020-08-21 05:07:55 +0600

    Updates

---
+++ precache-manifest.42014ad58e7b21de59f7cdbbf0345115.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea35d71071c36f1831f39fb1da2b6fe",
+    "url": "/chatapp/index.html"
+  },
+  {
+    "revision": "13cbffea3b86902a8f10",
+    "url": "/chatapp/static/css/main.24cd5cff.chunk.css"
+  },
+  {
+    "revision": "5e133987aa40fe217e4b",
+    "url": "/chatapp/static/js/2.b5ed20f2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/chatapp/static/js/2.b5ed20f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13cbffea3b86902a8f10",
+    "url": "/chatapp/static/js/main.ec2a045a.chunk.js"
+  },
+  {
+    "revision": "2902292036b6f59da77a",
+    "url": "/chatapp/static/js/runtime-main.de283741.js"
+  }
+]);
