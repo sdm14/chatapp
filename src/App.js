36ec3e435e6283195a81c6f051e1e9d67a96c919
sdm14 <149sdm10@gmail.com>
2020-08-21 05:02:07 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router-dom';
+import { Join } from './components/Join';
+import { General } from './components/General';
+import { Work } from './components/Work';
+
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path="/" exact component={Join} />
+      <Route path="/general" component={General} />
+      <Route path="/work" component={Work} />
     </div>
   );
 }
